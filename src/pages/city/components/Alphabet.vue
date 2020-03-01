e2c35248c1ccf8fd0ac80eac363df87989f595bd
smile-dyg <1261511437@qq.com>
2020-03-01 17:50:29 +0800

    add ajax in city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,37 +1,17 @@
 <template>
-  <ul class="list">
-      <li class="item">A</li>
-      <li class="item">B</li>
-      <li class="item">C</li>
-      <li class="item">D</li>
-      <li class="item">E</li>
-      <li class="item">F</li>
-      <li class="item">G</li>
-      <li class="item">H</li>
-      <li class="item">I</li>
-      <li class="item">J</li>
-      <li class="item">K</li>
-      <li class="item">L</li>
-      <li class="item">M</li>
-      <li class="item">N</li>
-      <li class="item">O</li>
-      <li class="item">P</li>
-      <li class="item">Q</li>
-      <li class="item">R</li>
-      <li class="item">S</li>
-      <li class="item">T</li>
-      <li class="item">U</li>
-      <li class="item">V</li>
-      <li class="item">W</li>
-      <li class="item">X</li>
-      <li class="item">Y</li>
-      <li class="item">Z</li>
+  <ul class="list" >
+      <li class="item" v-for="(item, key) of cities" :key="key">
+        {{key}}
+      </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
