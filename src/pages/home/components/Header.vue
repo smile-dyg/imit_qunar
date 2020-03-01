25e7310c5981ef6b8baf411ceabfe2efd68ee9b3
smile-dyg <1261511437@qq.com>
2020-03-01 13:50:31 +0800

    add CityHeader

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,9 +6,11 @@
       <div class="header-input">
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题</div>
-      <div class="header-right">{{this.city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
-      </div>
+      <router-link to='/city'>
+        <div class="header-right">{{this.city}}
+          <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+      </router-link>
   </div>
 </template>
 
@@ -48,6 +50,7 @@ export default {
       width: 1.24rem
       float: right
       text-align center
+      color #fff
       .arrow-icon
         margin-left -.04rem
         font-size: .24rem
